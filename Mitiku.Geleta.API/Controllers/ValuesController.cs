05124fc959b5a46c9263be7520ffbfd96820cd8c
Mitiku Geleta <mitikutesh@gmail.com>
2020-03-23 22:38:52 +0200

    add Resource api

---
+++ Mitiku.Geleta.API/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mitiku.Geleta.API.Controllers
+{
+    [Authorize(Policy = "ApiReader")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [Authorize(Policy = "Consumer")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new JsonResult(User.Claims.Select(c => new { c.Type, c.Value }));
+        }
+    }
+}
